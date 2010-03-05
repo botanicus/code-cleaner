fc7e69c64318322366e3335c6e44949a0df16c55
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-03-05 09:08:15 +0000

    New bundler

--- tasks.rb
+++ tasks.rb
@@ -1,6 +1,15 @@
 #!/usr/bin/env nake
 
 begin
+  require File.expand_path("../.bundle/environment", __FILE__)
+rescue LoadError
+  require "bundler"
+  Bundler.setup
+end
+
+$LOAD_PATH.unshift(File.expand_path("../lib", __FILE__))
+
+begin
   load "./tasks/code-cleaner.nake"
 rescue LoadError
   abort "If you want to contribute to code-cleaner, you have to run ./tasks.rb hooks:whitespace:install first!"
